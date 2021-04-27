c708239d0a0364a9d06519181ac09121fb92eede
EricPPerry <ericpaperry@gmail.com>
2021-04-26 22:16:13 -0400

    First commit Week 7 React homework

---
+++ car-inventory-app/src/components/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        <div>
+            <h1>Sign In Functionality Coming Soon!</h1>
+        </div>
+    )
+}
---
+++ car-inventory-app/src/components/SignUp/SignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignUp = () => {
+    return (
+        <div>
+            <h1>Sign Up Functionality Coming Soon! Stay tuned!</h1>
+        </div>
+    )
+}
